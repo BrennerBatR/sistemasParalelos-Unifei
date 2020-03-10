22e10ba4bb2dccf73889f0b6f5e52294ed093502
BrennerBatR <brenner.batista@outlook.com>
2020-03-10 20:34:03 -0300

    Soma paralela e sequencial

--- Aula2/Exemplo1.java
+++ Aula2/Exemplo1.java
@@ -1,32 +1,58 @@
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
-
-public class Exemplo1 {
+public class Exemplo1 extends Thread {
     private int resultado = 0;
+    private int id, inicio, fim;
 
-    private int Resultado() {
+    private Exemplo1(int id, int inicio, int fim) {
+        this.id = id;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public void run() {
+        soma(inicio, fim);
+        System.out.println("Parcial da thread " + id + " = " + Resultado());
+    }
+
+    public int Resultado() {
         return resultado;
     }
 
-    int soma(int inicio, int fim) {
-        int resultado = 0;
-        for (int i = inicio; i <= fim; i++) {
+    public void soma(int inicio, int fim) {
+        resultado = 0;
+        for (int i = inicio; i <= fim; i++)
             resultado += i;
-        }
-        return resultado;
+
     }
 
-    public static void main(String[] args) {
-        int inicio, fim;
+    public static void main(String[] args) throws InterruptedException {
+        Exemplo1 t1, t2;
+
+        int inicio, fim, passo;
         Scanner teclado = new Scanner(System.in);
-        Exemplo1 obj = new Exemplo1();
 
         System.out.println("Digite o inicio:");
         inicio = teclado.nextInt();
         System.out.println("Digite o fim:");
         fim = teclado.nextInt();
 
-        System.out.println("Soma = " + obj.soma(inicio, fim));
+        Exemplo1 obj = new Exemplo1(0, inicio, fim);
+
+        obj.soma(inicio, fim);
+        System.out.println("Soma final sequencial = " + obj.Resultado());
+
+        passo = (fim - inicio + 1) / 2;
+        t1 = new Exemplo1(1, inicio, inicio + passo);
+        t2 = new Exemplo1(2, inicio + passo + 1, fim);
+
+        t1.start();
+        t2.start();
+
+        t1.join(); // join serve para a thread principal ficar bloqueada ate a thread 2 retornar
+        t2.join();
+
+        System.out.println("Soma final paralela = " + (t1.Resultado() + t2.Resultado()));
+
     }
 }
